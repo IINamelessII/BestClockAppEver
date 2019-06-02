35bac7fd23bb4b477158ac88bb1fe63df54cf013
IINamelessII <justnitro26@gmail.com>
2019-06-02 16:16:42 +0300

    Create project, add randomized JSON response

---
+++ api/views.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timedelta
+from random import randint, random
+from django.http import JsonResponse
+from django.shortcuts import render
+
+
+def state(request):
+    now = datetime.now()
+    start_time = datetime(now.year, now.month, now.day + 1, 6, 0)
+    data = {
+        'works': random() > 0.2,
+        'repeat_counter': randint(0, 10),
+        'alert_time':  (start_time + timedelta(minutes=randint(0, 60))).strftime('%Y-%m-%d %H:%M'),
+        'power_from_network': random() > 0.5,
+    }
+    return JsonResponse(data)
